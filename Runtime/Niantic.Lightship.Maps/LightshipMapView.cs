a8e49439e74c716abda90559ee97a4a92ebe54e0
dawsv <137456756+dawsv@users.noreply.github.com>
2025-03-25 17:16:22 +0700

    Revert "Remove sqlite3 duplication"
    This reverts commit ca14642decb65dfb1537b1e32a97a9fc4ce60bc9.

--- Runtime/Niantic.Lightship.Maps/LightshipMapView.cs
+++ Runtime/Niantic.Lightship.Maps/LightshipMapView.cs
@@ -501,8 +501,6 @@ namespace Niantic.Lightship.Maps
    
         private bool isInBound(IMapTile mapTile)
         {
-            return true;
-
             var mapTileCoor = GetMapTileCoordinate(mapTile);
             Debug.Log($"mapTileCoor: {mapTileCoor.Latitude}, {mapTileCoor.Longitude}");
             if (mapTileCoor.Latitude >= MinBoundary.Latitude &&
